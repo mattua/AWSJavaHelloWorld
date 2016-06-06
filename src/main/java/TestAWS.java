c448a459055e2ed129529e4e788f5a1dffec21e6
Matt Thomas <Matt@Thomas.com>
2016-06-06 22:39:09 +0100

    Initial commit

---
+++ src/main/java/TestAWS.java
@@ -0,0 +1,20 @@
+import com.amazonaws.services.apigateway.AmazonApiGatewayClient;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+/**
+ * Created by mattua on 06/06/2016.
+ */
+public class TestAWS {
+
+    public static void main(String[] args){
+
+
+        AmazonApiGatewayClient client = new AmazonApiGatewayClient();
+
+        
+
+
+    }
+
+}
