4714a17ad4b26b4c3a175ea681bef2d5a0ec4803
Matt Thomas <matt.thomas@markit.com>
2016-06-07 08:48:02 +0100

    boom

--- src/main/java/TestAWS.java
+++ src/main/java/TestAWS.java
@@ -1,7 +1,6 @@
 import com.amazonaws.services.apigateway.AmazonApiGatewayClient;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 
+import com.amazonaws.regions.*;
 /**
  * Created by mattua on 06/06/2016.
  */
@@ -13,6 +12,8 @@ public class TestAWS {
         AmazonApiGatewayClient client = new AmazonApiGatewayClient();
 
         
+
+        System.out.println("boom");
 
 
     }
