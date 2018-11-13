049381971413ac7a24083f5532adcad5f963e790
Matt Thomas <matt.thomas@ihsmarkit.com>
2018-11-13 09:05:24 +0000

    removed creds

--- src/main/java/Credentials.java
+++ src/main/java/Credentials.java
@@ -6,7 +6,7 @@ public class Credentials {
 
     public static final String access_key_id= "AKIAJQBA6TTUVSE7ROHA";
 
-    public static final String secret_access_key ="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";
+    public static final String secret_access_key ="<>";
 
 
 
